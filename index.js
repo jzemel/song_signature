4f208bb1fe458d541fa94703816b690564d4adc3
Jonathan Zemel <jzemel@macbookair.lan>
2025-11-18 15:03:26 -0800

    new subtle season dividers layout

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let allTracks;
 // LAYOUT PARAMETERS
 
 // TODO replace this with automatic check of running locally or not
-const USE_LOCAL_DATA = true;
+const USE_LOCAL_DATA = false;
 const SHOWS_URL = USE_LOCAL_DATA 
     ? "/data/shows.json" 
     : "https://jzemel.github.io/song_signature/data/shows.json";
